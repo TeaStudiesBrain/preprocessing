8561f29e9d3dbbdfc0d921dcb53fc07ed5cde94f
TeaStudiesBrain <tucic.tea@gmail.com>
2024-12-02 17:17:33 +0100

    Update script_func_preprocessing.sh

--- script_func_preprocessing.sh
+++ script_func_preprocessing.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 #fMRI preprocessing script by Giacomo Handjaras, Francesca Setti 
 
-for run in 1 2 3 4 5 6 control
-
-do
 # despike
 3dDespike \
--prefix run"$run"_ds.nii.gz \
+-prefix run_ds.nii.gz \
 -NEW \
-run"$run".nii.gz
+run.nii.gz
 
 sleep 1;
 
 # time shift
 3dTshift  \
--prefix run"$run"_dsts.nii.gz \
+-prefix run_dsts.nii.gz \
 -tpattern seq+z \
-run"$run"_ds.nii.gz
+run_ds.nii.gz
 
 sleep 1;
 
@@ -26,63 +23,62 @@ then
 fi
 
 3dvolreg \
--prefix run"$run"_dstsvr.nii.gz \
+-prefix run_dstsvr.nii.gz \
 -twopass \
 -verbose \
 -base ref_run1.nii.gz \
--1Dfile run"$run"_motion.1D \
--maxdisp1D run"$run"_maxmov.1D \
-run"$run"_dsts.nii.gz
+-1Dfile run_motion.1D \
+-maxdisp1D run_maxmov.1D \
+run_dsts.nii.gz
 
 sleep 1;
 
 fsl_motion_outliers \
 -v \
--i run"$run"_dsts.nii.gz \
--o run"$run"_spikes.1D \
--s run"$run"_metric.txt \
--p run"$run"_plot.png \
+-i run_dsts.nii.gz \
+-o run_spikes.1D \
+-s run_metric.txt \
+-p run_plot.png \
 
 sleep 1;
 
 # smoothing to 6mm
-bet run"$run"_dstsvr.nii.gz run"$run"_dstsvrSS.nii.gz -F
+bet run_dstsvr.nii.gz run_dstsvrSS.nii.gz -F
 
-3dBlurToFWHM -prefix run"$run"_dstsvrsm6.nii.gz \
--input run"$run"_dstsvr.nii.gz \
--mask run"$run"_dstsvrSS_mask.nii.gz \
+3dBlurToFWHM -prefix run_dstsvrsm6.nii.gz \
+-input run_dstsvr.nii.gz \
+-mask run_dstsvrSS_mask.nii.gz \
 -FWHM 6 
 
 sleep 1;
 
 # scaling
-3dTstat -prefix run"$run"_preproc_mean_sm6.nii.gz \
-run"$run"_dstsvrsm6.nii.gz
+3dTstat -prefix run_preproc_mean_sm6.nii.gz \
+run_dstsvrsm6.nii.gz
 
-3dcalc -a run"$run"_dstsvrsm6.nii.gz \
--b run"$run"_preproc_mean_sm6.nii.gz \
+3dcalc -a run_dstsvrsm6.nii.gz \
+-b run_preproc_mean_sm6.nii.gz \
 -expr '(a/b)*100' \
 -datum float \
--prefix run"$run"_preproc_norm_sm6.nii.gz
+-prefix run_preproc_norm_sm6.nii.gz
 
 sleep 1;
 
-paste run"$run"_motion.1D run"$run"_metric.txt > run"$run"_no_interest.1D
+#paste run"$run"_motion.1D run"$run"_metric.txt > run"$run"_no_interest.1D
 
-done
 
-cat run1_no_interest.1D run2_no_interest.1D run3_no_interest.1D run4_no_interest.1D run5_no_interest.1D run6_no_interest.1D > allruns_no_interest.1D
+
+#cat run1_no_interest.1D run2_no_interest.1D run3_no_interest.1D run4_no_interest.1D run5_no_interest.1D run6_no_interest.1D > allruns_no_interest.1D
 
 # functional data of each run are detrended using a Savitzky-Golay filtering by running the matlab script "run_detrend.m"
 matlab -nodesktop -r 'run_detrend; exit'
 #"/mnt/c/Program Files/MATLAB/R2024b/bin/win64/matlab.exe" -nodesktop -r run_detrend; exit"
 
-for run in {1..6}
-do
-3dcopy run"$run"_sm6_detrend+orig run"$run"_sm6_detrend.nii.gz
+
+3dcopy run_sm6_detrend+orig run_sm6_detrend.nii.gz
 sleep 1
-rm -f run"$run"_sm6_detrend+orig*
-done
+rm -f run_sm6_detrend+orig*
+
 
 3dTcat -prefix allruns_preproc_norm_sm6_SG.nii.gz \
 run1_sm6_detrend.nii.gz \
